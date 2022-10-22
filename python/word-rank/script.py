90d7499bf31c2f2ca76a2738a8d87499a724fc14
hsierpinska <106077596+hsierpinska@users.noreply.github.com>
2022-10-22 23:57:44 +0200

    Update script.py

--- python/word-rank/script.py
+++ python/word-rank/script.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-
 # input: array with multiple strings
 # expected output: rank of the 3 most often repeated words in given set of strings and number of times they occured, case insensitive
 
@@ -26,6 +25,14 @@ sentences = [
 # 1. "mam" - 12
 # 2. "tak" - 5
 # 3. "z" - 2
+sentences_1d = [(j.lower()).rstrip(',') for sub in list(map(lambda a: a.split(" "), sentences)) for j in sub]
+set_sentences = set(sentences_1d)
+dict_sentences = { a : 0 for a in sentences_1d }
+for i in range(len(sentences_1d)):
+    if sentences_1d[i] in set_sentences:
+        dict_sentences[sentences_1d[i]]+=1
+list1 = (sorted(dict_sentences.items(), key=lambda item: item[1], reverse=True))[:3]
+[print(j) for i in list1 for j in i]
 
 
 # Good luck! You can write all the code in this file.
